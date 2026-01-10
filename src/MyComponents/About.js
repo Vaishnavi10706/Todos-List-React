82ba9441300a3da423c0e018a311f162f928d839
Vaishnavi10706 <vaishnavii10706@gmail.com>
2026-01-10 12:32:08 +0530

    Made: Todo list

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is the about section of MyTodosList
+      <p>
+        Hello everyone, this is a website name MyTodosList . In this website you can add and delete your todos. It is used to make a list of work which you need to do in a day or a week. now you do not need to remember all the task. You can write here and then save it .It will be saved foreven until you click on delete button. After completion of task delete the todo. 
+      </p>
+    </div>
+  )
+}
+
+export default About
