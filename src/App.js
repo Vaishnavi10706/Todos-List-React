e4954fbc7f7a1d5e899efa585f8189f37d878be0
Vaishnavi <vaishnavii10706@gmail.com>
2026-01-20 10:40:23 +0530

    Refactor App.js for cleaner code and structure

--- src/App.js
+++ src/App.js
@@ -5,15 +5,9 @@ import Todos from "./MyComponents/Todos";
 import AddTodo from "./MyComponents/AddTodo";
 import Footer from "./MyComponents/Footer";
 import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  // return <h1>Hello React is working ✅</h1>;
   let initTodo;
   if (localStorage.getItem("todos") === null) {
     initTodo = [];
@@ -24,54 +18,49 @@ function App() {
   const [todos, setTodos] = useState(initTodo);
 
   const onDelete = (todo) => {
-    console.log("I am ondelete of todo", todo);
-    setTodos(
-      todos.filter((e) => {
-        return e !== todo;
-      })
-    );
-    localStorage.setItem("todos", JSON.stringify(todos));
+    setTodos(todos.filter((e) => e !== todo));
   };
 
   const addTodo = (title, desc) => {
-    console.log("I am adding this todo", title, desc);
     let sno;
-    if (todos.length == 0) {
+    if (todos.length === 0) {
       sno = 1;
     } else {
       sno = todos[todos.length - 1].sno + 1;
     }
+
     const myTodo = {
-      sno: sno,
-      title: title,
-      desc: desc,
+      sno,
+      title,
+      desc,
     };
+
     setTodos([...todos, myTodo]);
-    console.log(myTodo);
   };
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
+
   return (
-    <>
     <Router>
       <Header title="My Todos List" searchBar={true} />
 
       <Routes>
-        <Route exact path="/" element = {
+        <Route
+          path="/"
+          element={
             <>
-            <AddTodo addTodo={addTodo} />
-            <Todos todos={todos} onDelete={onDelete} />
+              <AddTodo addTodo={addTodo} />
+              <Todos todos={todos} onDelete={onDelete} />
             </>
           }
         />
-        <Route exact path="/about" element={<About />}/>
+        <Route path="/about" element={<About />} />
       </Routes>
-      
-      <Footer/>
+
+      <Footer />
     </Router>
-    </>
   );
 }
 
