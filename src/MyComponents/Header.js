5f8f785c3821e42a0745dc062462e936390efa16
Vaishnavi <vaishnavii10706@gmail.com>
2026-01-20 10:47:02 +0530

    Refactor Header component to use Link instead of a tag

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
@@ -6,9 +6,12 @@ export default function Header(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+
+        {/* ✅ FIX HERE */}
+        <Link className="navbar-brand" to="/">
           {props.title}
-        </a>
+        </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -20,6 +23,7 @@ export default function Header(props) {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
@@ -29,17 +33,20 @@ export default function Header(props) {
               <Link className="nav-link" to="/about">About</Link>
             </li>
           </ul>
-          { props.searchBar ? <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form> : ""}
+
+          {props.searchBar ? (
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          ) : null}
         </div>
       </div>
     </nav>
@@ -48,10 +55,10 @@ export default function Header(props) {
 
 Header.defaultProps = {
   title: "Your Title Here",
-  searchBar: true
+  searchBar: true,
 };
 
 Header.propTypes = {
   title: PropTypes.string,
-  searchBar: PropTypes.bool.isRequired
-}
+  searchBar: PropTypes.bool.isRequired,
+};
